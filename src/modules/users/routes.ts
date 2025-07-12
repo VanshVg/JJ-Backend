8d8583e4399db55e37f306bf657e3d261dcf80ad
VanshVg <vansh.vg18@gmail.com>
2025-07-12 17:58:56 +0530

    added validation schema in add user address api

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,12 +1,19 @@
 import authMiddleware from "@/middlewares/auth.middleware";
 import { Router } from "express";
 import { addUserAddress, fetchUserProfile } from "./controller";
+import validationMiddleware from "@/middlewares/validation.middleware";
+import { addUserAddressSchema } from "./schema";
 
 const userRoutes = (): Router => {
   const userRouter = Router();
 
   userRouter.get("/users/profile", authMiddleware, fetchUserProfile);
-  userRouter.post("/users/address", authMiddleware, addUserAddress);
+  userRouter.post(
+    "/users/address",
+    authMiddleware,
+    validationMiddleware(addUserAddressSchema),
+    addUserAddress
+  );
 
   return userRouter;
 };
---
+++ src/modules/users/schema.ts
@@ -0,0 +1,16 @@
+import { AddressType } from "@/database/models/types/user-addresses.type";
+import { joiCommon } from "@/lib/validation-schema";
+import Joi from "joi";
+
+export const addUserAddressSchema = Joi.object({
+  address_line_1: joiCommon.joiString.label("Address line 1").required(),
+  address_line_2: joiCommon.joiString.label("Address line 2"),
+  landmark: joiCommon.joiString.label("Landmark"),
+  address_type: joiCommon.joiString
+    .label("Address type")
+    .allow(...Object.values(AddressType))
+    .required(),
+  is_primary: joiCommon.joiBoolean.required(),
+  longitude: joiCommon.joiNumber.label("Longitude").required(),
+  latitude: joiCommon.joiNumber.label("Latitude").required(),
+});
